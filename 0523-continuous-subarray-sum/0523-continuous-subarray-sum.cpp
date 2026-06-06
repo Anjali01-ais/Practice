3eb284d12bf89263ab0f423787e7f6c42560790a
Anjali Singh <aanjalisingh70@gmail.com>
2026-06-06 16:08:59 +0530

    Time: 82 ms (30.61%), Space: 146.2 MB (97.76%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+         unordered_map<int, int> mp;
+        mp[0] = -1;  // important
+
+        int prefixSum = 0;
+
+        for(int i = 0; i < nums.size(); i++) {
+            prefixSum += nums[i];
+
+            int rem = prefixSum % k;
+            if(rem < 0) rem += k;
+
+            if(mp.find(rem) != mp.end()) {
+                if(i - mp[rem] >= 2) {
+                    return true;
+                }
+            } else {
+                mp[rem] = i;  // store first occurrence
+            }
+        }
+
+        return false;
+    }
+};
