0427a3b3aac1642ab9307143d274feec6fdb43e2
Anjali Singh <aanjalisingh70@gmail.com>
2026-06-09 20:23:10 +0530

    Time: 8 ms (54.03%), Space: 21.3 MB (32.24%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+     int n=(int)heights.size();
+     vector<int> leftmin(n);
+        stack<int> st;
+
+        // Nearest smaller to left
+        for(int i = 0; i < n; i++) {
+            while(!st.empty() && heights[st.top()] >= heights[i]) {
+                st.pop();
+            }
+            if(st.empty()) leftmin[i] = 0;
+            else leftmin[i] = st.top() + 1;
+
+            st.push(i);
+        }
+
+        // clear stack
+        while(!st.empty()) st.pop();
+
+        vector<int> rightmin(n);
+
+        // Nearest smaller to right
+        for(int i = n - 1; i >= 0; i--) {
+            while(!st.empty() && heights[st.top()] >= heights[i]) {
+                st.pop();
+            }
+            if(st.empty()) rightmin[i] = n - 1;
+            else rightmin[i] = st.top() - 1;
+
+            st.push(i);
+        }
+
+        int maxA = 0;
+
+        for(int i = 0; i < n; i++) {
+            maxA = max(maxA, heights[i] * (rightmin[i] - leftmin[i] + 1));
+        }
+
+        return maxA;
+    }
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int n=(int)matrix.size();
+        int m=(int)matrix[0].size();
+        int maxA=0;
+        vector<int>height(m,0);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(matrix[i][j]=='1')height[j]++;
+                else height[j]=0;
+            }
+            int area=largestRectangleArea(height);
+            maxA=max(maxA,area);
+        }
+        return maxA;
+    }
+};
