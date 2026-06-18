08cbab93a8d565cd02bc5066b62cb4244a456264
Anjali Singh <aanjalisingh70@gmail.com>
2026-06-18 08:10:14 +0530

    Time: 0 ms (100%), Space: 7.7 MB (88.82%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.cpp
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -1,10 +1,15 @@
 class Solution {
 public:
     bool checkPerfectNumber(int num) {
-        long long sum=0;
-        for(int i=1; i<num; i++){
+        if(num==1) return false;
+        long long sum=1;
+        for(int i=2; i*i<=num; i++){
             if(num%i==0){
                 sum+=i;
+            
+            if(i!=num/i){
+                sum+=num/i;
+            }
             }
         }
         if(sum==num) return true;
