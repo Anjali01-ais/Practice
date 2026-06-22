6e1a1866e729595b160d0edfa962c85c1b233ead
Anjali Singh <aanjalisingh70@gmail.com>
2026-06-22 19:25:57 +0530

    Time: 0 ms (100%), Space: 38.4 MB (68.89%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -7,41 +7,21 @@ public:
             freq[c-'A']++;
         }
 
-        priority_queue<int> pq;
+         sort(freq.begin(), freq.end());
 
-        for(int f : freq){
-            if(f > 0) pq.push(f);
-        }
+    int maxFreq = freq[25];
+    int countMax = 1;
 
-        int time = 0;
+    // count how many have max frequency
+    for(int i = 24; i >= 0; i--){
+        if(freq[i] == maxFreq)
+            countMax++;
+        else
+            break;
+    }
 
-        while(!pq.empty()){
+    int ans = (maxFreq - 1) * (n + 1) + countMax;
 
-            vector<int> temp;
-
-            for(int i=0;i<n+1;i++){
-
-                if(!pq.empty()){
-
-                    int f = pq.top();
-                    pq.pop();
-
-                    f--;
-                    temp.push_back(f);
-                }
-            }
-
-            for(int &fq : temp)
-                if(fq>0){
-                pq.push(fq);    
-                }
-
-            if(pq.empty())
-                time += temp.size();
-            else
-                time += n+1;
-        }
-
-        return time;
+    return max((int)tasks.size(), ans);
     }
 };
