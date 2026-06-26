ff4886558a29ac98e9ea70c3c7de1134f3fc4240
Anjali Singh <aanjalisingh70@gmail.com>
2026-06-27 00:05:48 +0530

    Time: 1 ms (38.88%), Space: 22.1 MB (59.24%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+         int n = asteroids.size();
+        stack<int> st;
+
+        for (int i = 0; i < n; i++) {
+            bool dead = false;
+
+            while (!st.empty() && st.top() > 0 && asteroids[i] < 0) {
+
+                if (abs(st.top()) < abs(asteroids[i])) {
+                    st.pop();
+                    continue;
+                }
+                else if (abs(st.top()) == abs(asteroids[i])) {
+                    st.pop();
+                }
+
+                dead = true;
+                break;
+            }
+
+            if (!dead) {
+                st.push(asteroids[i]);
+            }
+        }
+
+        vector<int> ans(st.size());
+
+        for (int i = st.size() - 1; i >= 0; i--) {
+            ans[i] = st.top();
+            st.pop();
+        }
+
+        return ans;
+    }
+};
