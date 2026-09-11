9f3f555b4707fbccb2326ebef324527cda3f1ba1
Anjali Singh <aanjalisingh70@gmail.com>
2026-09-11 21:48:05 +0530

    Time: 0 ms (100%), Space: 8.6 MB (20.38%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int solve(int n,vector<int>&dp){
-        if(n==0) return 1;
         if(n<=3)return n;
         if(dp[n]!=-1) return dp[n];
         return dp[n]=solve(n-1,dp)+solve(n-2,dp);
